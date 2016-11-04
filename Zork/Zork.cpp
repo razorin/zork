46565e81dee1450b40ed6dde394ce1dd19dcd76e
razorin <razorinferri@gmail.com>
2016-11-04 20:28:36 +0100

    Added globals.h. Created very simple and basic input system of the game.

--- Zork/Zork.cpp
+++ Zork/Zork.cpp
@@ -2,10 +2,27 @@
 //
 
 #include "stdafx.h"
+#include <conio.h>
+#include <string>
+#include <iostream>
+#include "globals.h"
 
+
+using namespace std;
 
 int main()
 {
-    return 0;
+	string arguments = "";
+	char key = 0;
+	while (1) {
+		key = _getch();
+		if (key == KEYBOARD_KEY_ESCAPE)
+			break;
+		arguments += key;
+	}
+	
+
+
+	return 0;
 }
 
---
+++ Zork/globals.h
@@ -0,0 +1,7 @@
+#pragma once
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#define KEYBOARD_KEY_ESCAPE 27
+
+#endif // !GLOBALS_H
